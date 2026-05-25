ea3c6db44edd64071577e0af6a7f0302ca1aa340
JEONGONE\jeong <jeongone.min@gmail.com>
2026-05-25 13:27:36 +0900

    chore: spring boot, docker compose, mysql 초기 프로젝트 환경 구성

--- src/main/java/com/liveklass/liveklass/LiveklassApplication.java
+++ src/main/java/com/liveklass/liveklass/LiveklassApplication.java
@@ -1,13 +1,16 @@
 package com.liveklass.liveklass;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class LiveklassApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LiveklassApplication.class, args);
+        log.info("\n\n================================== Started ==================================\n\n");
     }
 
 }
